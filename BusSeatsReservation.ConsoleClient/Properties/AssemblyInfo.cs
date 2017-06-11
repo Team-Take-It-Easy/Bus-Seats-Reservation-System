26008ffa046e144de619eae1109a017d4143406a
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-06-11 20:28:01 +0300

    Removed BusSeatsReservation project and added BusSeatsReservation.ConsoleClient project

--- BusSeatsReservation.ConsoleClient/Properties/AssemblyInfo.cs
+++ BusSeatsReservation.ConsoleClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BusSeatsReservation")]
+[assembly: AssemblyTitle("BusSeatsReservation.ConsoleClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BusSeatsReservation")]
+[assembly: AssemblyProduct("BusSeatsReservation.ConsoleClient")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8973d27e-668e-458e-b33d-22cadf5394a6")]
+[assembly: Guid("31d5ab23-4efd-4a22-9452-29e3a852a1b4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BusSeatsReservation.ConsoleClient/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusSeatsReservation.ConsoleClient
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+        }
+    }
+}
